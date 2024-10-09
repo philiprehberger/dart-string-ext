ffbb998be6df600b989be7993e584311b1074b58
Philip Rehberger <me@philiprehberger.com>
2024-10-09 15:50:03 -0600

    Add primary barrel file matching package name

---
+++ lib/philiprehberger_string_ext.dart
@@ -0,0 +1,6 @@
+/// String extension methods for case conversion, truncation, masking, and validation.
+library;
+
+export 'src/case_extensions.dart';
+export 'src/manipulation_extensions.dart';
+export 'src/validation_extensions.dart';
